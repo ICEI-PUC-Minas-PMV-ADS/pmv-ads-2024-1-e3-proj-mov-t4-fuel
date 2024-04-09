d5f581ddef2f48487ba096ea92bee7d8bbddf4da
RickyPaulo <126209236+rickypaulo@users.noreply.github.com>
2024-04-09 19:56:42 +1000

    Criação do Input e Comentarios
    Criação dos Input NOME, EMAIL e TELEFONE, alem de comentários sobre o styles.

--- src/Fuel/componentes/meuperfil.js
+++ src/Fuel/componentes/meuperfil.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, Dimensions, Button, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, Button, TouchableOpacity, Alert, TextInput } from 'react-native';
 import { Appbar } from 'react-native-paper'; // Importe o Appbar do react-native-paper
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const MeuPerfil = () => {
    const [imagemPerfil, setImagemPerfil] = useState(null);
+   const [email, setEmail] = useState('');
+   const [telefone, setTelefone] = useState('');
+   const [nome, setNome] = useState('');
 
    const selecionarImagem = () => {
       ImagePicker.launchImageLibrary({}, resposta => {
@@ -21,7 +24,7 @@ const MeuPerfil = () => {
 
    return (
       <View style={styles.container}>
-        
+
          <View style={styles.logoContainer}>
             <TouchableOpacity onPress={() => console.log('Menu clicado')} style={styles.menuContainer}>
                <Image source={require('../Img/Icones/menu.png')} style={styles.menu} />
@@ -34,20 +37,42 @@ const MeuPerfil = () => {
             </TouchableOpacity>
          </View>
 
-         <Text style={styles.text}>Meu Perfil</Text>
+         <Text style={styles.textMeuPerfil}>Meu Perfil</Text>
 
-         <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Sair')}>
+
+         <TouchableOpacity style={styles.buttonSair} onPress={() => Alert.alert('Sair')}>
             <Image source={require('../Img/Icones/logout.png')} style={{ width: 24, height: 24, marginRight: 10 }} />
-            <Text style={styles.buttonText}>Sair</Text>
+            <Text style={styles.buttonSairText}>Sair</Text>
          </TouchableOpacity>
 
-         <TouchableOpacity style={styles.imageContainer} onPress={selecionarImagem}>
+         <TouchableOpacity style={styles.ContainerIcoFoto} onPress={selecionarImagem}>
             {imagemPerfil ? (
-               <Image source={{ uri: imagemPerfil }} style={styles.image} />
+               <Image source={{ uri: imagemPerfil }} style={styles.icoFoto} />
             ) : (
-               <Image source={require('../Img/Icones/addfoto.png')} style={styles.image} />
+               <Image source={require('../Img/Icones/addfoto.png')} style={styles.icoFoto} />
             )}
          </TouchableOpacity>
+
+         <TextInput //Campo Nome
+            style={styles.input}
+            placeholder='Nome'
+            onChangeText={text => setNome(text)}
+            value={nome}
+         />
+
+         <TextInput //Campo Telefone
+            style={styles.input}
+            placeholder='Telefone'
+            onChangeText={text => setTelefone(text)}
+            value={telefone}
+         />
+
+         <TextInput //Campo Email
+            style={styles.input}
+            placeholder='E-mail'
+            onChangeText={text => setEmail(text)}
+            value={email} 
+         />
 
          <Appbar style={styles.footer}>
             <Appbar.Action icon={require('../Img/Icones/homepage.png')} color="white" size={36} onPress={() => console.log('Home clicado')} style={styles.iconbar} />
@@ -61,6 +86,8 @@ const MeuPerfil = () => {
 };
 
 const styles = StyleSheet.create({
+
+   // Parte principal da tela, parte abaixo do ico Foto.
    container: {
       flex: 1,
       width: '100%',
@@ -68,11 +95,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: '#ffffff',
    },
+
+   //Logo Fuel
    logoContainer: {
       width: '100%',
       height: '13%',
       backgroundColor: '#00052F',
-      flexDirection: 'row', 
+      flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
    },
@@ -82,23 +111,29 @@ const styles = StyleSheet.create({
       resizeMode: 'center',
       alignItems: 'center',
    },
+
+   //Icone Menu
    menuContainer: {
       marginLeft: 15,
    },
+
    menu: {
       width: 30,
       height: 30,
       resizeMode: 'contain',
    },
+   //Icone Configurações
    configContainer: {
       marginRight: 15,
    },
+
    config: {
       width: 30,
       height: 30,
       resizeMode: 'contain',
    },
-   text: {
+   //Texto "meu perfil"
+   textMeuPerfil: {
       fontSize: 45,
       fontWeight: 'bold',
       alignItems: 'flex-start',
@@ -106,7 +141,21 @@ const styles = StyleSheet.create({
       color: '#272727',
       marginTop: 2,
    },
-   button: {
+
+   //Input Nome/Telefone/Email
+   input: {
+      
+      height: 50, // altura do campo
+      width: 350, //largura do campo
+      margin: 20, //espaço entre os campos
+      borderWidth: 2, //Borda da linha
+      padding: 10,
+      borderRadius: 35, //Arrendondando o campo
+      backgroundColor: '#EDEDED', //cor de fundo
+
+   },
+   //Botão de Sair/Logout
+   buttonSair: {
       flexDirection: 'row',
       width: '40%',
       paddingVertical: 10,
@@ -119,29 +168,34 @@ const styles = StyleSheet.create({
       bottom: 20,
       marginVertical: 85,
    },
-   buttonText: {
+   //Texto do Botão Sair/Logout
+   buttonSairText: {
       fontSize: 16,
       fontWeight: 'bold',
       color: 'black',
    },
-   imageContainer: {
+   //Container da parte onde esta o icone da foto
+   ContainerIcoFoto: {
       marginTop: 20,
    },
-   image: {
+   //Icone de Add Foto
+   icoFoto: {
       width: 70,
       height: 70,
    },
+   //Rodapé
    footer: {
-      width: '100%',
-      height: '10%',
+      width: '100%', //ajusta a largura do container rodape
+      height: '10%',//ajusta a altura do container rodape
       backgroundColor: '#00052F',
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: 'center', //Centraliza Verticalmente
+      alignItems: 'center', //Centraliza Horizontalmente
       bottom: 0,
       paddingBottom: 5,
       position: 'absolute',
-      flexDirection: 'row',
+      flexDirection: 'row', // Deixa os itens flexivel, 'row'vai deixa-los horizontalmente.
    },
+   //icones do rodapé
    iconbar: {
       marginHorizontal: 25,
    },
